8b5a64282478c4425e73e042a329b9b8cb6d0da6
AnnabelZhang1 <azhang22@stuy.edu>
2020-11-23 21:48:53 -0500

    added 'equals' and 'add'

---
+++ RealNumber.java
@@ -0,0 +1,58 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+  //---------ONLY EDIT BELOW THIS LINE------------
+
+  public boolean equals(RealNumber other){
+    if (value != 0)
+      {
+        double within = Math.abs(other.getValue() - value)/value;
+        if (within == 0.00001)
+          return true;
+        return false;
+      }
+    else
+      return value == other.value;
+  }
+
+  public RealNumber add(RealNumber other){
+     RealNumber sum = new RealNumber(other.getValue() + value);
+     return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the product of this and the other
+
+  public RealNumber multiply(RealNumber other){
+    return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this divided by the other
+
+  public RealNumber divide(RealNumber other){
+    return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this minus the other
+
+  public RealNumber subtract(RealNumber other){
+    return null;
+  }
+*/
+}
