d042e041beb5a6097df2e65c338f38a84a5085ae
AnnabelZhang1 <azhang22@stuy.edu>
2020-11-28 07:35:50 -0500

    extended to Number class and added 'compareTo'

---
+++ Number.java
@@ -0,0 +1,16 @@
+public abstract class Number{
+
+  public abstract double getValue();
+    /*return 0 when this Number equals the other RealNumber
+      return a negative value when this Number is smaller than the other Number
+      return a positive value when this Number is larger than the other Number
+      */
+  public int compareTo(Number other){
+      if ((this.getValue())==(other.getValue()))
+        return 0;
+      else if ((this.getValue()) < (other.getValue()))
+        return -1;
+      else
+        return 1;
+  }
+}
