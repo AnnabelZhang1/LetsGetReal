0e44ce0ec9759464cc86f46fea3564b9001fb7af
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-27 17:06:07 -0500

    fixed gcd and arithmetic methods

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,114 +1,92 @@
-public class RationalNumber extends RealNumber
-{
+public class RationalNumber extends RealNumber{
   private int numerator, denominator;
 
-  /**Initialize the RationalNumber with the provided values
-  *  if the denominator is 0, make the fraction 0/1 instead
-  *  If the denominator is negative, negate both numerator and denominator
-  *@param nume the numerator
-  *@param deno the denominator
-  */
-  public RationalNumber(int nume, int deno)
-    {
-      super(0.0);//this value is ignored!
-      if (deno == 0)
-        {
-          numerator = 0;
-          denominator = 1;
-        }
-        else if (deno < 0)
-        {
-          numerator = -1 * nume;
-          denominator = -1 * deno;
-        }
-        else
-        {
-          numerator = nume;
-          denominator = deno;
-        }
+  public RationalNumber(int nume, int deno){
+    super(0.0); //this value is ignored!
+    if (deno == 0){
+      numerator = 0;
+      denominator = 1;
+    }
+    else if (deno < 0){
+      numerator = -1 * nume;
+      denominator = -1 * deno;
+    }
+    else{
+      numerator = nume;
+      denominator = deno;
+      reduce();
+    }
+  }
+
+  public double getValue(){
+    return (((double)numerator) / ((double)denominator));
+  }
+
+  public int getNumerator(){
+    return numerator;
+  }
+
+  public int getDenominator(){
+    return denominator;
+  }
+
+  public RationalNumber reciprocal(){
+    RationalNumber recip = new RationalNumber(denominator, numerator);
+    return recip;
+  }
+
+  public boolean equals(RationalNumber other){
+    return other.getValue() == this.getValue();
+  }
+
+  public String toString(){
+   if (numerator == 0)
+      return "0";
+   else if (denominator == 1)
+      return "" + numerator;
+    return numerator + "/" + denominator;
+  }
+
+  private static int gcd(int a, int b){
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (a == 0 || b == 0)
+      return 1;
+    else if (a < b){
+        int temp = a;
+        a = b;
+        b = temp;
       }
+    int remainder = a % b;
+    while (remainder != 0){
+        a = b;
+        b = remainder;
+        remainder = a % b;
+      }
+    return b;
+  }
 
-  public double getValue()
-    {
-      return (((double)numerator) / ((double)denominator));
-    }
+  private void reduce(){
+    int greatestcd = gcd(numerator, denominator);
+    numerator = numerator / greatestcd;
+    denominator = denominator / greatestcd;
+  }
 
-  public int getNumerator()
-    {
-      return numerator;
-    }
+  public RationalNumber multiply(RationalNumber other){
+    return new RationalNumber(numerator * other.getNumerator(), denominator * other.getDenominator());
+  }
 
-  public int getDenominator()
-    {
-      return denominator;
-    }
+  public RationalNumber divide(RationalNumber other){
+    return new RationalNumber(numerator * other.getDenominator(), denominator * other.getNumerator());
+  }
 
-  public RationalNumber reciprocal()
-    {
-      RationalNumber recip = new RationalNumber(denominator, numerator);
-      return recip;
-    }
+  public RationalNumber add(RationalNumber other){
+    return new RationalNumber(numerator * other.getDenominator() + other.getNumerator() * denominator, denominator * other.getDenominator());
+  }
 
-  public boolean equals(RationalNumber other)
-    {
-      return other.getValue() == this.getValue();
-    }
-
-  public String toString()
-    {
-      return numerator + "/" + denominator;
-    }
-
-  private static int gcd(int a, int b)
-    {
-      a = Math.abs(a);
-      b = Math.abs(b);
-      if (a < b)
-        {
-          int temp = b;
-          a = b;
-          b = temp;
-        }
-      int remainder = a % b;
-      while (remainder != 0)
-        {
-          remainder = a % b;
-          a = b;
-          b = remainder;
-        }
-      return a;
-    }
-
-  private void reduce()
-    {
-      int greatestcd = gcd(numerator, denominator);
-      numerator = numerator / greatestcd;
-      denominator = denominator / greatestcd;
-    }
-
-  public RationalNumber multiply(RationalNumber other)
-    {
-      RationalNumber product = new RationalNumber(numerator * other.getNumerator(), denominator * other.getDenominator());
-      return product;
-    }
-
-  public RationalNumber divide(RationalNumber other)
-    {
-      RationalNumber quotient = new RationalNumber(numerator * other.getDenominator(), denominator * other.getNumerator());
-      return quotient;
-    }
-
-  public RationalNumber add(RationalNumber other)
-    {
-      RationalNumber sum = new RationalNumber(numerator + other.getNumerator(), denominator + other.getDenominator());
-      return sum;
-    }
-
-  public RationalNumber subtract(RationalNumber other)
-    {
-      RationalNumber difference = new RationalNumber((numerator * other.getDenominator()) - other.getNumerator(), denominator * other.getDenominator());
-      return difference;
-    }
+  public RationalNumber subtract(RationalNumber other){
+    return new RationalNumber(numerator * other.getDenominator() - other.getNumerator() * denominator, denominator * other.getDenominator());
+  }
 
   public static void main(String[] args)
     {
