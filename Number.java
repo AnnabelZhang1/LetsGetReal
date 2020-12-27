622095d4e7403df55bc163b6ec0bd2a6a12e80fb
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-27 18:00:11 -0500

    changed equals to within 0.00001%

--- Number.java
+++ Number.java
@@ -1,10 +1,7 @@
 public abstract class Number{
 
   public abstract double getValue();
-    /*return 0 when this Number equals the other RealNumber
-      return a negative value when this Number is smaller than the other Number
-      return a positive value when this Number is larger than the other Number
-      */
+
   public int compareTo(Number other){
       if ((this.getValue())==(other.getValue()))
         return 0;
@@ -15,10 +12,9 @@ public abstract class Number{
   }
 
   public boolean equals(Number other){
-    if (this.getValue() != 0)
-      {
+    if (this.getValue() != 0){
         double within = (Math.abs(this.getValue() - other.getValue())) / this.getValue();
-        if (within <= 0.00001)
+        if (Math.abs(within) <= 0.0000001)
           return true;
         return false;
       }
